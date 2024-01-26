3651eff423c58b4691d72f5df063522bfc9a13e5
jabooo188 <secretoadivina01@gmail.com>
2024-01-25 20:06:07 -0600

    cambios post solucion

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-let numSecret = Math.floor(Math.random() * 100)+1;
+//variables
+let numeroMax = 100;
+let numSecret = Math.floor(Math.random() * numeroMax)+1;
 let num1 = 0;
 let intentos = 1;
 let maxIntentos = 4;
@@ -9,7 +11,7 @@ let maxIntentos = 4;
 //comparacion del numero
 while (numSecret != num1, numSecret > 1) {
     
-    let num1 = parseInt(prompt('Adivina el número entre 1 y 100'));
+    let num1 = parseInt(prompt(`Adivina el número entre 1 y ${numeroMax}`));
 
     if (num1 == numSecret){
         alert(`Exacto! ese es el numero. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
@@ -24,7 +26,7 @@ while (numSecret != num1, numSecret > 1) {
         //palabraVeces = 'veces'
 
         if (intentos > maxIntentos){
-            alert(`Excediste el numero de intentos. El numero es ${numSecret}`);
+            alert(`Excediste el numero de intentos. El numero secreto es ${numSecret}`);
             break;
         }
     };
